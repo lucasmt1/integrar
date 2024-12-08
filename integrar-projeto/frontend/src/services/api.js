d3c49689bacc54484c29b1aefe735101859e56e4
Lucas Miranda <lmirandabandeira@gmail.com>
2024-12-07 21:49:54 -0300

    arrumando render

--- integrar-projeto/frontend/src/services/api.js
+++ integrar-projeto/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000',
+    baseUrl: process.env.REACT_APP_API_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
